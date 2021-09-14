a178e0be7c08413137cdde78ded00a2fb8ba74b2
Even Wei <2940573604@qq.com>
2021-09-14 11:41:55 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -3,8 +3,8 @@ import java.util.ResourceBundle;
 
 public class Greeter {
 
-  private Locale locale;
-  private String name;
+  private final Locale locale;
+  private final String name;
 
   public Greeter(String language, String country, String name) {
     locale = new Locale(language, country);
@@ -17,10 +17,15 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+    try{
     String language = args[0];
     String country = args[1];
     String name = args[2];
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
+  }
+    catch (ArrayIndexOutOfBoundsException e){
+      System.out.println("Exception throw :" + e);
+    }
   }
 }
